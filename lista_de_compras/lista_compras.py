4818826c864ed25fbf622c35269842af7b987028
Victor Facundo <victorfacundo122@hotmail.com>
2023-05-10 11:17:00 -0300

    atualização da lista de compras/carrinho

--- lista_de_compras/lista_compras.py
+++ lista_de_compras/lista_compras.py
@@ -1,28 +1,45 @@
-lista_de_compras = []
-compra = True
-while compra:
-    escolha = input('Deseja adicionar itens?[i]. Deseja apagar itens[a]. Deseja listar itens?[l]. Finalizar a lista[f]: ')
-    if escolha == 'i' or escolha == 'I':
-        produto = input('Digite o produto: ')
-        lista_de_compras.append(produto)
-        print(lista_de_compras)
-        print("")
+# carrinho de compras 
+product_list = []
+purchase = True
+total = 0.0
 
-    elif escolha == 'a' or escolha == 'A':
-        indice = input('Qual item deseja excluir? Escolha o indice: ')
-        del lista_de_compras[int(indice)]
-        print("")
-            
-    elif escolha == 'l' or escolha == 'L':
-        for numero, item in enumerate(lista_de_compras):
-            print(numero, item)
-            continue
-            print("")
-            
-    elif escolha == "f" or escolha == "F":
-        print("Programa finalizado !\n")
-        break
-     
-    else:
-        print("Opção inválida! \n")
-        pass
+while purchase:
+    print('------ carrinho de compras ------ ')
+    option = input('press (B) to buy, press (F) to finalize, press (E) to erase: ')
+        
+    if option == 'b' or option == 'B':
+        product = input('Which product do you want to add?: ')
+        product_list.append(product)
+
+        price = float(input('value of product: '))
+        amount = float(input('How many:'))
+        total_many = price*amount
+        total += total_many
+    
+
+    elif option == 'f' or option == 'F':
+        print(f'current price = {total}')
+        for index, list in enumerate(product_list):
+            print(index, list)
+            break
+
+    
+    elif option == 'e' or option == 'E':
+        indice = input('Qual indice deseja excluir: ')
+        indice = int(indice)
+        for index, list in enumerate(product_list):
+            print(index, list)
+        
+        del product_list[indice]
+        # reduzindo valor
+
+        less_price = float(input('value of product: '))
+        less_how = float(input('how many: '))
+
+        less_total_many = less_price * less_how
+        total  -=  less_total_many
+
+    else: 
+        print('digite uma opção valida')        
+       
+
